e0fd9cac8a8812c10edb3c4ccdb1523c58f32683
Matt Karro <mpkarro@gmail.com>
2022-03-07 10:41:48 -0600

    Fixes found by code review

--- S3Upload.java
+++ S3Upload.java
@@ -45,6 +45,16 @@ public class S3Upload {
         } catch (IOException e) {
             System.out.println("Something went wrong");
         }
+		finally {
+			if (reader != null){
+				try {
+					reader.close();
+				}
+				catch (IOException e){
+					System.out.printf("Something went wrong");
+				}
+			}
+		}
 
         return fileContents;
     }
